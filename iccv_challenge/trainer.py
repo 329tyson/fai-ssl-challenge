31509b40e15cc2411c527760bea43a4cd085c2c5
Taeyoung Son <ty.son@postech.ac.kr>
2019-08-10 15:19:36 +0900

    implement stable trainer for voc w/ alexnet

---
+++ iccv_challenge/trainer.py
@@ -0,0 +1,98 @@
+import torch
+
+import numpy as np
+import torch.nn as nn
+import torch.optim as optim
+import pytorch_lightning as pl
+import torchvision.models as models
+
+from pytorch_lightning import Trainer
+from test_tube import Experiment
+
+from voc_loader import load_voc
+from utils import LightningConfig
+from utils import AverageMeter
+
+
+class Basic_Trainer(pl.LightningModule):
+    def __init__(self, config: LightningConfig):
+        # do some initialization
+        super(Basic_Trainer, self).__init__()
+        self.model = models.alexnet(pretrained=config.pretrained)
+        self.criterion = nn.BCELoss().cuda()
+
+        # Reconfigure last classifier for fine-tuning
+        # FIXME newly added layer might need some init work?
+        self.model.classifier[6] = nn.Linear(4096, 20)
+        self.model.cuda()
+
+    def forward(self, x):
+        # forward propagation
+        return self.model(x)
+
+    def training_step(self, batch, batch_nb):
+        # one training step for one batch, return type in dict
+        x, y = batch
+        x_val = x.cuda()
+        y_val = y.cuda().float()
+
+        output = self.forward(x_val)
+        output = nn.Sigmoid()(output)
+
+        return {"loss": self.criterion(output, y_val)}
+
+    def validation_step(self, batch, batch_nb):
+        # one validation step for one batch, return type in dict
+        x, y = batch
+        x_val = x.cuda()
+        y_val = y.cuda().float()
+
+        output = self.forward(x_val)
+        output = nn.Sigmoid()(output)
+        loss = self.criterion(output, y_val)
+
+        return {"val_loss": loss.item()}
+
+    def validation_end(self, outputs):
+        # integrate outputs for one validation epoch, return type in dict
+        validation_loss = AverageMeter()
+        for loss_dict in outputs:
+            validation_loss.update(loss_dict["val_loss"], 1)
+        return {"avg_val_loss": validation_loss.avg}
+
+    def configure_optimizers(self):
+        # return list of optimizers
+        optimizer = optim.SGD(self.parameters(), lr=0.01)
+        scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20)
+        return [optimizer], [scheduler]
+
+    @pl.data_loader
+    def tng_dataloader(self):
+        return load_voc("train")
+
+    @pl.data_loader
+    def val_dataloader(self):
+        return load_voc("val")
+
+    @pl.data_loader
+    def test_dataloader(self):
+        return load_voc("test")
+
+
+def main():
+    model = Basic_Trainer(
+        LightningConfig(
+            imagepath="",
+            train_labelpath="",
+            valid_labelpath="",
+            pretrained=True,
+        )
+    )
+    exp = Experiment(save_dir="runs/alexnet_voc07", name="FirstBaseline")
+    trainer = Trainer(experiment=exp)
+    trainer.fit(model)
+    exp.save()
+
+
+if __name__ == "__main__":
+    main()
---
+++ iccv_challenge/voc_loader.py
@@ -0,0 +1,109 @@
+import os
+
+import torch.utils.data as data
+import torchvision.transforms as transforms
+import numpy as np
+
+from utils import getlogger
+from utils import DatasetConfig
+from datasets.pil_augmentation import get_image
+from datasets.transforms import normalize
+
+
+class VOC_DATASET(data.Dataset):
+    def __init__(self, config: DatasetConfig):
+        self.image_path = config.imagepath
+        self.label_path = config.labelpath
+        self.preprocess = transforms.Compose(config.preprocess)
+        self.images = []
+
+        self.load_images()
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, index):
+        imagepath, label = self.images[index]
+        image = get_image(imagepath)
+
+        return self.preprocess(image), label
+
+    def load_images(self):
+        # for fai-ssl-challenge dataset, you need to use numpy formatted image, label file
+        assert self.image_path.endswith(".npy")
+        assert self.label_path.endswith(".npy")
+
+        image_file = np.load(self.image_path)
+        label_file = np.load(self.label_path)
+
+        for image, label in zip(image_file, label_file):
+            self.images.append((image, label))
+
+
+def _voc_loader(image_path: str, label_path: str, batch_size=128, shuffle=True):
+    '''
+    Args:
+        image_path : path to numpy file describes images
+        label_path : path to numpy file describes labels
+    '''
+    logger = getlogger()
+    image_file = np.load(image_path)
+    label_file = np.load(label_path)
+
+    logger.info(f"Loading image from {image_path}")
+    logger.info(f"\t corresponding label from {label_path}")
+
+    # imagefile and labelfile are should be the same in length
+    assert len(image_file) == len(label_file)
+
+    # define data augmentation
+    preprocess = [
+        transforms.Resize((256, 256)),
+        transforms.CenterCrop((224, 224)),
+        transforms.ToTensor(),
+        normalize(),
+    ]
+
+    logger.info(f"Applying {preprocess}")
+
+    # build dataset
+    voc_dataset = VOC_DATASET(
+        DatasetConfig(
+            imagepath=image_path,
+            labelpath=label_path,
+            preprocess=preprocess,
+        )
+    )
+
+    # build dataloader and return
+    return data.DataLoader(
+        voc_dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=True,
+        num_workers=8,
+    )
+
+
+def load_voc(type: str):
+    voc_train_image_path = os.environ.get("VOC_TRAIN_IMAGE")
+    voc_train_label_path = os.environ.get("VOC_TRAIN_LABEL")
+
+    voc_val_image_path = os.environ.get("VOC_VAL_IMAGE")
+    voc_val_label_path = os.environ.get("VOC_VAL_LABEL")
+
+    voc_test_image_path = os.environ.get("VOC_TEST_IMAGE")
+    voc_test_label_path = os.environ.get("VOC_TEST_LABEL")
+
+    type = type.lower()
+    if type == "train":
+        return _voc_loader(voc_train_image_path, voc_train_label_path)
+
+    elif type == "val":
+        return _voc_loader(voc_val_image_path, voc_val_label_path)
+
+    elif type == "test":
+        return _voc_loader(voc_test_image_path, voc_test_label_path)
+
+    else:
+        raise NotImplementedError
